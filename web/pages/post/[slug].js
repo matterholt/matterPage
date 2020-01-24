051fb02a86d8755d13445a2a2b6fa72a13cddcd6
MAtterholt <matterholt84@gmail.com>
2020-01-23 22:07:31 -0500

    added author image

--- web/pages/post/[slug].js
+++ web/pages/post/[slug].js
@@ -1,7 +1,18 @@
 import groq from "groq";
+import imageUrlBuilder from "@sanity/image-url";
 import client from "../../client";
+
+function urlFor(source) {
+  return imageUrlBuilder(client).image(source);
+}
+
 const Post = props => {
-  const { title = "Missing title", name = "Missing Name", categories } = props;
+  const {
+    title = "Missing title",
+    name = "Missing Name",
+    categories,
+    authorImage
+  } = props;
   return (
     <article>
       <h1>{title}</h1>
@@ -14,6 +25,15 @@ const Post = props => {
           ))}
         </ul>
       )}
+      {authorImage && (
+        <ul>
+          <img
+            src={urlFor(authorImage)
+              .width(75)
+              .url()}
+          />
+        </ul>
+      )}
     </article>
   );
 };
@@ -21,7 +41,8 @@ const Post = props => {
 const query = groq`*[_type == "post" && slug.current == $slug][0]{
   title,
   "name": author ->name,
-  "categories": categories[]-> title
+  "categories": categories[]-> title,
+  "authorImage" : author->image
 }`;
 
 Post.getInitialProps = async function(context) {
