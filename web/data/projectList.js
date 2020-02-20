a657c9c8c070456d03ccf8df4d78e3f42ce58ed7
MAtterholt <matterholt84@gmail.com>
2020-02-19 22:25:43 -0500

    update content

--- web/data/projectList.js
+++ web/data/projectList.js
@@ -1,52 +1,13 @@
 const repos = [
   {
-    title: "FEA result tracking",
-    tags: ["Python", "Pandas"],
-    content:
-      "Program was create to capture results during development. The program was built to allow engineers to\
-      copy list results into a template and upon save would create a excel report that can sent out to the team.\
-      The real power of the program is that result and model info are saved to a database, allowing the possibility\
-       of developing more features",
-    link: "https://github.com/matterholt/result_project_tracking"
-  }
-];
-export default repos;
-
-const hold_need_Check = [
-  ,
-  {
-    title: "Abaqus Static curve script",
-    content:
-      "A script that works with the Abaqus results odb. It has a library that is specific and only works with Python2",
-    link: "https://github.com/matterholt/abaqusStaticCurveCompiler"
-  },
-
-  {
-    title: "Python work scripts",
-    content:
-      "Where I gotten my start and found the ways to create scripts to help\
-      with the task with our daily work. There are a handful of script that\
-       would assist other engineers. Script consist of ",
-    link: "https://github.com/matterholt/work"
-  },
-  {
     title: "Work Hour Log",
     content:
-      "We worked in logging the hours with excel. Creating a front end web app to calculate the hours for the week. Primary language was JavaScript with REACT.js",
+      "We worked in logging the hours with excel. \
+      Creating a front end web app to calculate the hours for the week.\
+       Primary language was JavaScript with REACT.js",
     link: "https://github.com/matterholt/workHourLog"
   },
-  {
-    title: "Weld Thickness Calculator",
-    content:
-      "Simple calculator that calculates the thickness of the weld for a simulation",
-    link: "https://github.com/matterholt/WeldThicknessCalculator"
-  },
-  {
-    title: "Build Me",
-    content:
-      "A project that is started up with a co-worker. We started this project to learn the full stack development. Where my primary role is to work on the backend",
-    link: "https://github.com/TEAM-BUILDME/tool-share-backend"
-  },
+
   {
     title: "FEA tracking system",
     content:
@@ -57,5 +18,48 @@ const hold_need_Check = [
         tracking application\
       and apply it our work flow.",
     link: "https://github.com/matterholt/fea_tracker"
+  },
+  {
+    title: "FEA result tracking",
+    tags: ["Python", "Pandas"],
+    content:
+      "Program was create to capture results during development. The program was built to allow engineers to\
+      copy list results into a template and upon save would create a excel report that can sent out to the team.\
+      The real power of the program is that result and model info are saved to a database, allowing the possibility\
+       of developing more features",
+    link: "https://github.com/matterholt/result_project_tracking"
+  },
+  {
+    title: "Abaqus Static curve script",
+    content:
+      "A script that works with the Abaqus results odb. \
+      It has a library that is specific and only works with Python2",
+    link: "https://github.com/matterholt/abaqusStaticCurveCompiler"
+  },
+
+  {
+    title: "Weld Thickness Calculator",
+    content:
+      "Simple calculator that calculates the thickness of the weld for a simulation",
+    link: "https://github.com/matterholt/WeldThicknessCalculator"
+  },
+  {
+    title: "Build Me",
+    content:
+      "A project that is started up with a co-worker.\
+       We started this project to learn the full stack development.\
+        Where my primary role is to work on the backend",
+    link: "https://github.com/TEAM-BUILDME/tool-share-backend"
+  },
+  {
+    title: "Python work scripts",
+    content:
+      "Where I gotten my start and found the ways to create scripts to help\
+      with the task with our daily work. There are a handful of script that\
+       would assist other engineers. Script consist of ",
+    link: "https://github.com/matterholt/work"
   }
 ];
+export default repos;
+
+const hold_need_Check = [,];
--- web/pages/about.js
+++ web/pages/about.js
@@ -11,16 +11,18 @@ const Content = () => {
           with building relational tables using sqlite and PostgreSQL.
         </p>
         <p>
+          I took this brain type quiz, I am an alchemist, meaning the things
+          that bring me excitement is exploring different possibilities and
+          curious about new things, discovering interesting patterns across
+          multiple context. Coding is a good playground where I can thrive in
+          discovery and variety. To sum it up, a Jack of all trades.
+        </p>
+
+        <p>
           Work as a Validation Technician for 14 years. Started to learn Python
           to help automate task that would slow down and annoy the team. As my
           knowledge grew so did my desire to help with other task and that could
           bring more structure to the group.
-        </p>
-        <p>
-          I have worked on a 68 mustang that was drivable for my wedding. Later
-          I able to build a small black smith shop at my house, where I take
-          scrap steel and pound it in to shape. I enjoy building and working
-          with wood as well.
         </p>
       </div>
     </div>
