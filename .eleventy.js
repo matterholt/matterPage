3c3f3e4edac39d2441723e9a436ea074ab8241a4
MAtterholt <matterholt84@gmail.com>
2021-02-25 17:48:48 -0500

    scss

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,15 @@
 // const pluginSass = require('"eleventy-plugin-sass"')
-
+const
+    dev = (global.dev = process.env.ELEVENTY_ENV === "development");
+    now = new Date()
 
 module.exports = function (config) {
     //    config.addPlugin(pluginSass, sassPluginOptions)
  
     config.addPlugin(require("@11ty/eleventy-navigation"));
     config.addShortcode('navlist', require('./lib/shortcode/navlist.js'))
+    config.addTransform('postcss', require('./lib/transforms/postcss'))
+    config.addWatchTarget('./src/scss/')
 
 
     return{
