bd8b3545e3f0c0970d85accb5cef1859758e1a55
matterholt <matterholt.dev@gmail.com>
2023-05-13 14:07:29 -0400

    make it better

---
+++ src/utils/helpers_build/currentNav.js
@@ -0,0 +1,29 @@
+export default function activeNavigationPage(pageId){
+    let navigationLinks = [
+        {
+          id: "home",
+          link: "\\",
+          title: "home",
+          activeClass: pageId == "home" ? "current" : "",
+        },        {
+            id: "writeups",
+            link: "\\writeups",
+            title: "write ups",
+            activeClass: pageId == "writeups" ? "current" : "",
+          },
+        {
+          id: "career",
+          link: "\\career",
+          title: "career",
+          activeClass: pageId == "career" ? "current" : "",
+        },
+        {
+          id: "projects",
+          link: "\\projects",
+          title: "projects",
+          activeClass: pageId == "projects" ? "current" : "",
+        },
+
+      ];
+      return navigationLinks
+}
