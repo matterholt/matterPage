22aa2d0d6bd89ef7888bc4c15a22463c27c2b9df
matterholt <matterholt.dev@gmail.com>
2023-12-08 00:06:42 -0500

    working on redesign

---
+++ src/components/lib_comps/index.ts
@@ -0,0 +1,10 @@
+import Alink from "./a_link/Alink.astro";
+import Heading from "./Heading.astro";
+import PostIntro from "./post_intro/PostIntro.astro";
+import PostListings from "./post_listings/PostListings.astro";
+import Logo from "./logo/Logo.astro";
+import BGImageBase from "./bg_image_base/BGImageBase.astro";
+
+import Text from "./text/Text.astro";
+
+export { BGImageBase, Alink, Heading, Text, PostIntro, PostListings, Logo };
---
+++ src/layouts/index.ts
@@ -0,0 +1,3 @@
+import PageLayout from "./page_layout/PageLayout.astro";
+
+export { PageLayout };
