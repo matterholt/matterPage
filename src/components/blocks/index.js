2dc0e1c85cc6efa77518cf0d343bc1018ea67913
matterholt <matterholt.dev@gmail.com>
2023-12-17 20:50:09 -0500

    latest write ups

--- src/components/blocks/index.js
+++ src/components/blocks/index.js
@@ -2,7 +2,9 @@ import SectionContainer from "./SectionContainer.astro"
 import JobPositions from "./JobPositions.astro"
 import ProjectDetail from "./ProjectDetail.astro"
 import NavBar from "./nav_bar/NavBar.astro"
+import InternalLink from "./internal_link/InternalLinkl.astro"
+import DateIt from "./date_it/DateIt.astro"
 
 export {
-    SectionContainer, JobPositions, ProjectDetail, NavBar
+    SectionContainer, JobPositions, ProjectDetail, NavBar, InternalLink, DateIt
 }
