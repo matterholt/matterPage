81864207f444b5c1870b9c6e6bcc038c714ab233
matterholt <matterholt.dev@gmail.com>
2024-01-03 08:37:38 -0500

    clean up comps, make comps

--- src/components/lib_comps/index.ts
+++ src/components/lib_comps/index.ts
@@ -4,7 +4,17 @@ import PostIntro from "./post_intro/PostIntro.astro";
 import PostListings from "./post_listings/PostListings.astro";
 import Logo from "./logo/Logo.astro";
 import BGImageBase from "./bg_image_base/BGImageBase.astro";
+import LinkBreak from "./line_break/LineBreak.astro";
 
 import Text from "./text/Text.astro";
 
-export { BGImageBase, Alink, Heading, Text, PostIntro, PostListings, Logo };
+export {
+  BGImageBase,
+  Alink,
+  Heading,
+  Text,
+  PostIntro,
+  PostListings,
+  Logo,
+  LinkBreak,
+};
