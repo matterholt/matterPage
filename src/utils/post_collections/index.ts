4fa7e685474b9549c9d570155f9d5013116255a7
matterholt <matterholt.dev@gmail.com>
2024-01-03 23:42:44 -0500

    working on redesign

--- src/utils/post_collections/index.ts
+++ src/utils/post_collections/index.ts
@@ -7,11 +7,16 @@ function sortDates(dateList) {
 async function filterCollectionsBy(collectionType) {
   const allBlogPosts = await getCollection("blog");
   const yearToday = new Date().getFullYear();
+
   const filterByCurrentYear = allBlogPosts.filter((x) => {
     const year = x.data.pubDate.getFullYear();
     return year === yearToday;
   });
-  let latest = sortDates(filterByCurrentYear);
+
+  if (filterByCurrentYear.length === 0) {
+  }
+
+  let latest = sortDates(allBlogPosts);
 
   if (collectionType === "most_recent") {
     return latest[0];
