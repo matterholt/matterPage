d033df731e8440b4db9737f58742e6e2a588e0c1
matterholt <matterholt.dev@gmail.com>
2024-02-05 22:22:23 -0500

    updates and workings

--- src/utils/post_collections/index.ts
+++ src/utils/post_collections/index.ts
@@ -1,29 +1,52 @@
 import { getCollection, getEntry } from "astro:content";
+const __Years_Of_Content = new Set();
 
 function sortDates(dateList) {
   return dateList.sort((a, b) => b.data.pubDate - a.data.pubDate);
 }
 
-async function filterCollectionsBy(collectionType) {
-  const allBlogPosts = await getCollection("blog");
-  const yearToday = new Date().getFullYear();
-
-  const filterByCurrentYear = allBlogPosts.filter((x) => {
+async function getLatestByYear(checkedYear: number, contentData: []) {
+  // decrement year till values are found
+  const filterByCurrentYear = await contentData.filter((x) => {
     const year = x.data.pubDate.getFullYear();
-    return year === yearToday;
+    return year === checkedYear;
   });
 
-  if (filterByCurrentYear.length === 0) {
+  if (filterByCurrentYear.length > 0) {
+    return checkedYear;
+  }
+  getLatestByYear(checkedYear - 1, contentData);
+}
+
+function sortByYears(completeBlogContent) {
+  const writeupsPerYear = new Map();
+
+  for (const x of completeBlogContent) {
+    let contentDate = x.data.pubDate.getFullYear();
+    __Years_Of_Content.add(contentDate);
+    if (writeupsPerYear.has(contentDate)) {
+      let yearContent = writeupsPerYear.get(contentDate);
+      writeupsPerYear.set(contentDate, [...yearContent, x]);
+      continue;
+    }
+    writeupsPerYear.set(contentDate, [x]);
   }
 
-  let latest = sortDates(allBlogPosts);
+  return writeupsPerYear;
+}
 
-  if (collectionType === "most_recent") {
-    return latest[0];
-  }
-  if (collectionType === "current_year") {
-    return latest;
-  }
+function groupByCurrentandRest() {
+  // group post by latest of year and
+}
+
+async function filterCollectionsBy(collectionType) {
+  const allBlogPosts = await getCollection("blog");
+
+  const writeUpByYears = sortByYears(allBlogPosts);
+
+  const mostRecentandRest = groupByCurrentandRest(allBlogPosts);
+  // return most recent year, and then the rest
+  console.log([...writeUpByYears]);
 
   return allBlogPosts;
 }
