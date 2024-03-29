9ff4ed0df3385ef1b7d392f8e1462104eb76876e
matterholt <matterholt.dev@gmail.com>
2024-03-29 08:17:22 -0400

    footer

--- src/components/lib_comps/index.ts
+++ src/components/lib_comps/index.ts
@@ -4,8 +4,9 @@ import PostIntro from "./post_intro/PostIntro.astro";
 import Logo from "./logo/Logo.astro";
 import BGImageBase from "./bg_image_base/BGImageBase.astro";
 import LinkBreak from "./line_break/LineBreak.astro";
+import BentoBox from "./bento_box/BentoBox.astro";
 
 import Text from "./text/Text.astro";
 import Box from "./box/Box.astro";
 
-export { BGImageBase, Box, Alink, Heading, Text, PostIntro, Logo, LinkBreak };
+export { BGImageBase, Box, Alink, Heading, Text, PostIntro, Logo, BentoBox,LinkBreak };
