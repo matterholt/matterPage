27382c617155e09e1175b88f51522baa0df59cfc
matterholt <matterholt.dev@gmail.com>
2024-03-29 11:58:46 -0400

    desktop layout and design

--- src/components/blocks/index.js
+++ src/components/blocks/index.js
@@ -5,8 +5,10 @@ import NavBar from "./nav_bar/NavBar.astro"
 import InternalLink from "./internal_link/InternalLinkl.astro"
 import DateIt from "./date_it/DateIt.astro"
 import BackgroundImage from "./background_image/BackgroundImage.astro"
+import SocialLinkCode from "./social_link_code/SocialLinkCode.astro"
+
 
 export {
-    BackgroundImage,
+    BackgroundImage,SocialLinkCode,
     SectionContainer, JobPositions, ProjectDetail, NavBar, InternalLink, DateIt
 }
