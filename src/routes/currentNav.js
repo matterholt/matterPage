9b449ef8b78d6201f8d4d9a13914ed24c537c950
matterholt <matterholt.dev@gmail.com>
2024-04-01 23:43:09 -0400

    navigation clean up and other things

--- src/routes/currentNav.js
+++ src/routes/currentNav.js
@@ -4,24 +4,28 @@ export default function activeNavigationPage(pageId) {
       id: "home",
       link: "\\",
       title: "home",
+      isPublish: true,
       activeClass: pageId == "home" ? "current" : "",
     },
     {
       id: "writeups",
       link: "\\writeups",
       title: "write ups",
+      isPublish: true,
       activeClass: pageId == "writeups" ? "current" : "",
     },
     {
       id: "career",
       link: "\\career",
       title: "career",
+      isPublish: true,
       activeClass: pageId == "career" ? "current" : "",
     },
     {
       id: "projects",
       link: "\\projects",
       title: "projects",
+      isPublish: false,
       activeClass: pageId == "projects" ? "current" : "",
     },
   ];
--- src/routes/mainNavigation.ts
+++ src/routes/mainNavigation.ts
@@ -2,11 +2,12 @@ interface Navigations {
   title: string;
   icon: string;
   path: string;
+  isPublish: boolean;
 }
 
 export const mainNavigation: Navigations[] = [
-  { title: "home", icon: "./", path: "/" },
-  { title: "write ups", icon: "./", path: "/writeups" },
-  { title: "career", icon: "./", path: "/career" },
-  { title: "projects", icon: "./", path: "/projects" },
+  { title: "home", icon: "./", path: "/", isPublish: true },
+  { title: "write ups", icon: "./", path: "/writeups", isPublish: true },
+  { title: "career", icon: "./", path: "/career", isPublish: true },
+  { title: "projects", icon: "./", path: "/projects", isPublish: false },
 ];
