c61c7dd5d334a9b6af2457a2ba4ed4d96e3705b1
matterholt <matterholt.dev@gmail.com>
2024-04-06 16:43:06 -0400

    astro build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "server",
-  adapter: netlify()
 });
