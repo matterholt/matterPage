d3624623a9c0799a1c8d414810129084cd70913f
matterholt <matterholt.dev@gmail.com>
2025-03-10 23:27:34 -0400

    build fixes

--- front/src/content/content.config.ts
+++ front/src/content/content.config.ts
@@ -1,12 +1,13 @@
 // define collections
 // 1. Import utilities from `astro:content`
-import { defineCollection, z } from "astro:content";
-import heldJobPosition from "./schema/heldJobPosition.ts"
-;
+import { defineCollection} from "astro:content";
+import heldJobPosition from "./schemas/jobPosition";
+import { glob } from 'astro/loaders';
+
 const positions = defineCollection({
   /* ... */
-  loader:glob({pattern:"**/*.md", base:"./src/content/positions"})
-  schema: heldJobPosition,
+  loader:glob({pattern:"**/*.md", base:"./src/content/positions"}),
+  schema: heldJobPosition
 });
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
