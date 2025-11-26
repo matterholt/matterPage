9ff930cefaa0bdd15d4196b2c65ef32458fa61b7
matterholt <matterholt.dev@gmail.com>
2025-11-26 07:52:11 -0500

    working on articles tags collections

---
+++ front/src/utils/article_actions.ts
@@ -0,0 +1,23 @@
+
+export function articlePublish(data) {
+  const buildDate = new Date();
+
+  const isProducts = import.meta.env.PROD;
+
+  const afterPublishDate = new Date(data.publishDate) <= buildDate;
+
+  return afterPublishDate;
+}
+
+
+export function firstParagraph(content) {
+  return content.split("\n").filter((y) => y.length > 0)[0];
+}
+
+
+export function collectAllTags(blogPosts): string[] {
+  // takes the blog post for all tag that are active tags
+
+  const allTags = blogPosts.flatMap(post => post.data.tags || []);
+  return [...new Set(allTags)];
+}
