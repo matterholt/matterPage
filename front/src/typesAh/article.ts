01fcfccc93d9e59ac71463eaca8ff17975338cbc
matterholt <matterholt.dev@gmail.com>
2025-11-26 22:51:04 -0500

    building out article

---
+++ front/src/typesAh/article.ts
@@ -0,0 +1,25 @@
+export interface Article {
+  id: string;
+  data: {
+    title: string;
+    slug: string;
+    publishDate: Date;
+    tags: string[];
+  };
+  body: string;
+  filePath: string;
+  digest: string;
+  rendered: {
+    html: string;
+    metadata: {
+      headings: any[];
+      localImagePaths: string[];
+      remoteImagePaths: string[];
+      frontmatter: Record<string, any>;
+      imagePaths: string[];
+    };
+  };
+  collection: string;
+  slug: string;
+  render: () => void;
+}
