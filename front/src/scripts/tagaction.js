22f618ff6657aebd83fff5ee5afecc495e9a26b6
matterholt <matterholt.dev@gmail.com>
2025-12-02 22:46:20 -0500

    some scripts

--- front/src/scripts/tagaction.js
+++ front/src/scripts/tagaction.js
@@ -1,4 +1,4 @@
-
+import { resetTags, handleSelectedTags } from './tagClass.js'
 const tagCollectionUnorderList = document.getElementsByClassName("taglist");
 
 for (let unorderList of tagCollectionUnorderList) {
@@ -9,65 +9,10 @@ for (let unorderList of tagCollectionUnorderList) {
 let selectedTag = [];
 
 
-function collectDom(targetTag) {
-  return document.getElementsByClassName(targetTag.toLowerCase())
-}
-
-function handleSelectedTags(event) {
-  const targetTag = event.target.innerText;
-  const cssAction = updateSelectedTag(targetTag)
-  handleCssClass(targetTag, cssAction)
-  evealuate();
-};
-
-
-function updateSelectedTag(targetTag) {
-  if (selectedTag.includes(targetTag)) {
-    selectedTag = [...selectedTag].filter((x) => x !== targetTag);
-    return "remove"
-  } else {
-    selectedTag = [...selectedTag, targetTag];
-  }
-  return 'add'
-}
-
-
-function handleCssClass(targetTag, action) {
-  const allTargetedTags = collectDom(targetTag)
-  for (let tag of allTargetedTags) {
-    if (action === 'remove') {
-      tag.classList.remove("selected_tag");
-    }
-    tag.classList.add("selected_tag");
-  }
-}
-
-
-function evealuate() {
-  console.log("check slecelted tags");
-  console.log(selectedTag);
-  blogit()
-}
-
-
 function lisentersOnTag(listItems) {
   listItems.forEach((element) =>
     element.addEventListener("click", handleSelectedTags),
   );
-}
-
-function resetTags() {
-  if (selectedTag.length === 0) {
-    return
-  }
-  console.log("reset", selectedTag)
-  for (let tag of selectedTag) {
-    const collection = document.getElementsByClassName(tag.toLowerCase())
-    for (let collect of collection) {
-      collect.classList.remove("selected_tag");
-    }
-  }
-  selectedTag = []
 }
 
 const resetTagButton = document.getElementById("resetTag")
@@ -76,21 +21,11 @@ resetTagButton.addEventListener("click", resetTags)
 
 // temp
 
-
-
-
 // below remove from file and place into other 
 // main index file and pull in modules
 
 function articleTags(post) {
-
-  const tagit = post.getElementsByClassName("taglist")
-  const litag = tagit.querySelectorAll('li')
-  // const listContent = Array.from(litag).map(x => x.textContent)
-  debugger
-
-
-  return tagit
+  return Array.from(post.querySelectorAll('li')).map(x => x.innerText.trim())
 }
 
 
@@ -98,41 +33,33 @@ function articleTags(post) {
 
 let orginalCollection = [];
 
+
 function blogit() {
+
   const parentDom = document.getElementById("articleContent")
   const articleCollection = parentDom.getElementsByClassName("postBlock")
-  if (orginalCollection.length === 0) [
+
+  if (orginalCollection.length === 0) {
     orginalCollection = [...articleCollection]
-  ]
-
-  const newElement = []
-
-  for (let article of orginalCollection) {
-    /**
-     *
-     *
-     * 1. take orginal collection 
-     * 2. collect the tag set for each article
-     * 3. if any tag match the selected_tag
-     * 4. return any article that has a selected_tag
-     *
-     */
-    const tagging = orginalCollection
-    console.log("article", tagging)
-    debugger
-
-
-
-    if (false) {
-      newElement.push(article)
-    }
-
   }
 
-  console.log("newElement", newElement)
-  console.log("orginal", orginalCollection)
-  console.log("parent", parentDom)
 
+  const setTag = new Set(selectedTag)
+  const newElement = []
+
+  for (const post of orginalCollection) {
+    const blogTagList = new Set(articleTags(post))
+    const commonTags = blogTagList.intersection(setTag)
+    if (commonTags.size > 0) {
+      debugger
+
+      newElement.push(post)
+    }
+
+    console.log("BASE ARTi", orginalCollection)
+    console.log("new ARTi", newElement)
+
+  }
   parentDom.innerHTML = ''
   parentDom.append(...newElement);
 
