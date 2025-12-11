d2d1db337e1d1a02d908b91665b273f32a782151
matterholt <matterholt.dev@gmail.com>
2025-12-11 13:33:05 -0500

    UI for tag for article listing

--- front/src/typesAh/article.ts
+++ front/src/typesAh/article.ts
@@ -23,3 +23,18 @@ export interface ArticleCollection {
   slug: string;
   render: () => void;
 }
+// {"frontmatter":{"title":"Things that will be install on the next machine.",
+// "slug":"new-machine-installs",
+//"publishDate":"2025-11-17T00:00:00.000Z",
+//"tags":["terminal","TUI","installs","setup","dev_environment"]},
+//"file":"/home/matterholt/Work/sites/matterPage/front/src/content/articles/2025-11-17_Standard_Installs_2025.md"}
+//
+interface ArticleFrontMatter {
+  title: string;
+  slug: string;
+  publishDate: string;
+  tags: string[];
+}
+export interface FrontMatterContent {
+  frontmatter: ArticleFrontMatter
+}
