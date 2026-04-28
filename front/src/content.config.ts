9009723c33e41a2e4cebc6d93f9d21771e795b5a
MAtterholt <matterholt.dev@gmail.com>
2026-04-27 22:22:51 -0400

    astro updates

--- front/src/content.config.ts
+++ front/src/content.config.ts
@@ -1,23 +1,31 @@
-import { jobAccomplishments, jobPosition, personalArticles } from "./schemas";
+import {
+  jobAccomplishments,
+  jobPosition,
+  personalArticles,
+} from "./content/schemas";
 
 import { defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
 
 const positions = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./src/content/positions" }),
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/positions" }),
   schema: jobPosition,
 });
 const accomplishments = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./src/content/accomplishments  " }),
+  loader: glob({
+    pattern: "**/*.{md,mdx}",
+    base: "./src/content/accomplishments  ",
+  }),
   schema: jobAccomplishments,
 });
 const articles = defineCollection({
   loader: glob({
-    pattern: "**/*.md",
+    pattern: "**/*.{md,mdx}",
     base: "./src/content/personalArticles  ",
   }),
   schema: personalArticles,
 });
+
 export const collections = {
   positions,
   accomplishments,
